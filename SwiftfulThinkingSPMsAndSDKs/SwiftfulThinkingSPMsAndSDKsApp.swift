640a2848386c9d66b3d60e0d2bc671c918296f3c
Artur Remizov <arturremizov@gmail.com>
2024-05-11 18:57:11 +0300

    Initial commit

---
+++ SwiftfulThinkingSPMsAndSDKs/SwiftfulThinkingSPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingSPMsAndSDKsApp.swift
+//  SwiftfulThinkingSPMsAndSDKs
+//
+//  Created by Artur Remizov on 11.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingSPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
